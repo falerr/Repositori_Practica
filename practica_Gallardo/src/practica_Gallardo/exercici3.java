a437039a277fe8deeeabc42d1a75c7bef4e53834
guifregallardo <guifregallardo@gmail.com>
2021-12-20 10:33:24 +0100

    esborrar print two

--- practica_Gallardo/src/practica_Gallardo/exercici3.java
+++ practica_Gallardo/src/practica_Gallardo/exercici3.java
@@ -5,11 +5,9 @@ public class exercici3
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
 	}
 
 	public static void printOne() {
 		System.out.println("Hello World");
 	}
-
 }
