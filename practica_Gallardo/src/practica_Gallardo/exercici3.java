39a8ae8b7c7e4755cb473399e63a407cd5182483
guifregallardo <guifregallardo@gmail.com>
2021-12-20 10:28:38 +0100

    canvis

---
+++ practica_Gallardo/src/practica_Gallardo/exercici3.java
@@ -0,0 +1,19 @@
+package practica_Gallardo;
+
+public class exercici3
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
